1ff54f6ff1ad0b3b04d6d6fcaef99c2b9c7ad8b8
Ivan Guimarães Farias <iguimaraesf@gmail.com>
2023-11-18 07:43:00 -0300

    First commit

---
+++ internal/service/category.go
@@ -0,0 +1,33 @@
+package service
+
+import (
+	"context"
+
+	"github.com/iguimaraesf/14-grpc/internal/database"
+	"github.com/iguimaraesf/14-grpc/internal/pb"
+)
+
+type CategoryService struct {
+	pb.UnimplementedCategoryServiceServer
+	CategoryDB database.Category
+}
+
+func NewCategoryService(categoryDB database.Category) *CategoryService {
+	return &CategoryService{
+		CategoryDB: categoryDB,
+	}
+}
+
+// precisa de uma assinatura extata para implementar a interface
+func (c *CategoryService) CreateCategory(ctx context.Context, in *pb.CreateCategoryRequest) (*pb.CategoryResponse, error) {
+	category, err := c.CategoryDB.Create(in.Name, in.Description)
+	if err != nil {
+		return nil, err
+	}
+	categoryResponse := &pb.Category{
+		Id:          category.ID,
+		Name:        category.Name,
+		Description: category.Description,
+	}
+	return &pb.CategoryResponse{Category: categoryResponse}, nil
+}
